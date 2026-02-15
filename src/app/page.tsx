2c11c1af80eb63121a1801dfa23315873026a308
sakshamrepuria-lang <sakshamrepuria@gmail.com>
2026-02-15 12:34:39 +0530

    added blog section and restored full layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import ShopByConcern from '../components/homepage/ShopByConcern';
 import BestSellers from '../components/homepage/BestSellers';
 import ScienceSection from '../components/homepage/ScienceEducation'; // <--- IMPORT THIS
 import Footer from '../components/layout/Footer';
+import BlogSection from '../components/homepage/BlogSection';
+import Reviews from '../components/homepage/Reviews';
 
 export default function Home() {
   return (
@@ -15,6 +17,8 @@ export default function Home() {
         <ShopByConcern />
         <ScienceSection />  {/* <--- ADD THIS HERE */}
         <BestSellers />
+        <Reviews /> 
+        <BlogSection />   {/* 5. The Blog Grid */}
       </div>
 
       <Footer />
